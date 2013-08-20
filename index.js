8c50f34eec509f2d70469314031d0b94b13b4735
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-08-20 08:49:14 -0400

    testing

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var connect = require('connect')
   , http = require('http');
 
+var boggle = require('boggle');
+// solve a dummy boggle to force dictionary load
+boggle('zzzz zzzz zzzz zzzz');
+
 var port = 3000;
 
 var app = connect()
@@ -8,8 +12,20 @@ var app = connect()
   .use(connect.logger('dev'))
   .use(connect.static('public'))
   .use(connect.directory('public'))
+  .use(connect.query())
   .use(function(req, res){
-    res.end('Hello from Connect!\n');
+    if (req.query.letters) {
+      var str = decodeURIComponent(req.query.letters);
+      console.log('solving', str);
+      var words = boggle(str);
+      console.log('writing response', words);
+      var response = JSON.stringify(words);
+      res.writeHead(200, {
+        'Content-Length': response.length,
+        'Content-Type': 'text/plain' });
+      res.write(response);
+      res.end();
+    }
   });
 
 http.createServer(app).listen(port);
